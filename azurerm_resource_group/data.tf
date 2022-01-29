ff6c95b51d77b2b6da345e002b602cb721d57319
Woody <29414889+woowil@users.noreply.github.com>
2022-01-29 19:59:30 +0100

    Update data.tf

--- azurerm_resource_group/data.tf
+++ azurerm_resource_group/data.tf
@@ -3,11 +3,3 @@ data "azurerm_resource_group" "rg" {
 
   depends_on = [azurerm_resource_group.rg]
 }
-
-data "azuread_users" "members" {
-  count = length(var.contributor_members) > 1 ? 1 : 0
-
-  user_principal_names = var.contributor_members
-
-  ignore_missing = true
-}
