147e6bec4b1099f582b56609cfa41b402f234812
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-02-09 23:43:30 +0100

    Updated Static Web Site

--- azurerm_static_site/resources.tf
+++ azurerm_static_site/resources.tf
@@ -5,13 +5,14 @@ resource "azurerm_static_site" "ss" {
 
   name                = var.name
   resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
+  location            = var.location
   sku_tier            = var.sku_tier
   sku_size            = var.sku_size
   tags                = var.tags
 
   identity {
-    type = "SystemAssigned "
+    type = "SystemAssigned"
+    identity_ids = var.identity_ids
   }
 
 }
--- azurerm_static_site/variables.tf
+++ azurerm_static_site/variables.tf
@@ -1,11 +1,20 @@
 variable "name" {}
 variable "rg_name" {}
+variable "location" {
+  type        = string
+  description = "The Location for Static website. List of available regions are: westus2,centralus,eastus2,westeurope or eastasia"
+  default     = "westeurope"
+  validation {
+    condition     = contains(["westus2", "centralus", "eastus2", "westeurope", "eastasia"], var.location)
+    error_message = "Variable \"location\" must either be westus2, centralus, eastus2, westeurope or eastasia."
+  }
+}
 variable "sku_tier" {
   type        = string
   description = "- (Optional) Specifies the sku tier of the Static Web App. Possible values are Free or Standard. Defaults to Free."
   default     = "Free"
   validation {
-    condition     = contains(["Free", "Standard"], var.sku_size)
+    condition     = contains(["Free", "Standard"], var.sku_tier)
     error_message = "Variable \"sku_tier\" must either be \"Free\", or \"Standard\"."
   }
 }
@@ -17,6 +26,11 @@ variable "sku_size" {
     condition     = contains(["Free", "Standard"], var.sku_size)
     error_message = "Variable \"sku_size\" must either be \"Free\", or \"Standard\"."
   }
+}
+variable = "identity_ids" {
+  type = list(string)
+  description = "(Optional) A list of Managed Identity ID's which should be assigned to this Static Site resource."
+  default = []
 }
 variable "tags" {
   type        = map(any)
