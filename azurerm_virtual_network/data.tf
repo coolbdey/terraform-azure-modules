348d098a33de80acb21b7372a46e21cbbc4fc3a7
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-02-19 00:24:42 +0100

    updated module

--- azurerm_virtual_network/data.tf
+++ azurerm_virtual_network/data.tf
@@ -1,10 +1,3 @@
-data "azurerm_client_config" "current" {}
 data "azurerm_resource_group" "rg" {
   name = var.rg_name
-}
-data "azurerm_virtual_network" "vnet" {
-  depends_on = [azurerm_virtual_network.vnet]
-
-  name                = var.name
-  resource_group_name = azurerm_virtual_network.vnet.resource_group_name
 }
--- azurerm_virtual_network/resources.tf
+++ azurerm_virtual_network/resources.tf
@@ -2,7 +2,7 @@
 
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
 resource "azurerm_virtual_network" "vnet" {
-  depends_on = [data.azurerm_resource_group.rg, azurerm_network_ddos_protection_plan.dpp]
+  depends_on = [data.azurerm_resource_group.rg]
 
   name                    = var.name
   resource_group_name     = data.azurerm_resource_group.rg.name
@@ -10,8 +10,8 @@ resource "azurerm_virtual_network" "vnet" {
   address_space           = var.address_space
   dns_servers             = local.dns_servers
   tags                    = var.tags
-  bgp_community           = 12076 # (Optional) The BGP community attribute in format <as-number>:<community-value>.
   flow_timeout_in_minutes = var.flow_timeout_in_minutes
+  #bgp_community           = 12076 # (Optional) The BGP community attribute in format <as-number>:<community-value>.
 
   dynamic "ddos_protection_plan" {
     for_each = var.ddospp_id != null ? [1] : []
