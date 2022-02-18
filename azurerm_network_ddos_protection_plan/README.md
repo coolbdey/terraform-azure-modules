69c5f73d07f984e93165d2316383b8c089cf3fb0
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-02-19 00:01:47 +0100

    Added module azurerm_network_ddos_protection_plan

---
+++ azurerm_network_ddos_protection_plan/README.md
@@ -0,0 +1,8 @@
+## 
+
+### Module info
+---
+
+* Azure only allows one DDoS Protection Plan per region.
+* The DDoS Protection Plan is a high-cost service. Please keep this in mind while testing and learning.
+https://azure.microsoft.com/en-us/pricing/details/ddos-protection/#pricing
---
+++ azurerm_network_ddos_protection_plan/resources.tf
@@ -0,0 +1,13 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_ddos_protection_plan
+resource "azurerm_network_ddos_protection_plan" "dpp" {
+  depends_on = [data.azurerm_resource_group.rg]
+
+  name                = var.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  tags                = var.tags
+
+  lifecycle {
+    ignore_changes = [location]
+  }
+}
