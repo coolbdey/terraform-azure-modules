68e8baf6091eb91893f12c5778fb4db24af4fb32
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-02-18 23:59:53 +0100

    Updated module azurerm_virtual_network

--- azurerm_virtual_network/resources.tf
+++ azurerm_virtual_network/resources.tf
@@ -1,43 +1,35 @@
 
 
-# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_ddos_protection_plan
-resource "azurerm_network_ddos_protection_plan" "dpp" {
-  depends_on = [data.azurerm_resource_group.rg]
-
-  name                = var.dpp_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-
-  lifecycle {
-    ignore_changes = [location]
-  }
-}
-
-resource "azurerm_network_watcher" "nw" {
-  depends_on = [data.azurerm_resource_group.rg]
-
-  name                = var.nw_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-
-  lifecycle {
-    ignore_changes = [location]
-  }
-}
-
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
 resource "azurerm_virtual_network" "vnet" {
   depends_on = [data.azurerm_resource_group.rg, azurerm_network_ddos_protection_plan.dpp]
 
-  name                = var.name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  address_space       = var.address_space
-  dns_servers         = local.dns_servers
-  tags                = var.tags
+  name                    = var.name
+  resource_group_name     = data.azurerm_resource_group.rg.name
+  location                = var.location == null ? data.azurerm_resource_group.rg.location : var.location
+  address_space           = var.address_space
+  dns_servers             = local.dns_servers
+  tags                    = var.tags
+  bgp_community           = 12076 # (Optional) The BGP community attribute in format <as-number>:<community-value>.
+  flow_timeout_in_minutes = var.flow_timeout_in_minutes
 
-  ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.dpp.id
-    enable = var.dpp_enabled
+  dynamic "ddos_protection_plan" {
+    for_each = var.ddospp_id != null ? [1] : []
+    iterator = each
+    content {
+      id     = var.ddospp_id
+      enable = var.ddospp_enabled
+    }
+  }
+
+  dynamic "subnet" {
+    for_each = length(var.subnets) > 0 ? var.subnets : []
+    iterator = each
+    content {
+      name           = each.value.name
+      address_prefix = each.value.address_prefix
+      security_group = each.value.security_group_id
+    }
   }
 
   lifecycle {
