789fb047ceb41b7fd7e6c669d810754af1505a76
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-03-05 02:35:25 +0100

    Updated outputs

--- azurerm_key_vault/outputs.tf
+++ azurerm_key_vault/outputs.tf
@@ -1,24 +1,24 @@
 output "name" {
   description = "key vault name"
-  value       = data.azurerm_key_vault.kv.name
+  value       = var.name
 }
 
 output "id" {
   description = "key vault id"
-  value       = data.azurerm_key_vault.kv.id
+  value       = azurerm_key_vault.kv.id
 }
 
 output "url" {
   description = "key vault url"
-  value       = data.azurerm_key_vault.kv.vault_uri
+  value       = azurerm_key_vault.kv.vault_uri
 }
 
 output "key_ids" {
   description = "key vault key ids"
-  value       = [for item in data.azurerm_key_vault_key.key: item.id]
+  value       = [for item in azurerm_key_vault_key.key: item.id]
 }
 
 output "secret_ids" {
   description = "key vault key secrets"
-  value       = [for item in data.azurerm_key_vault_secret.secret: item.id]
+  value       = [for item in azurerm_key_vault_secret.secret: item.id]
 }
