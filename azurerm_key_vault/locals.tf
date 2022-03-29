604451f50c4ac0046a42318b5b6dcf8acfe4a7b8
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-03-29 06:33:46 +0200

    Update 03/29/2022 06:33:46 2

--- azurerm_key_vault/locals.tf
+++ azurerm_key_vault/locals.tf
@@ -2,11 +2,6 @@ locals {
   interpreter          = ["PowerShell", "-Command"]
   is_selected_networks = var.network_acls.default_action == "Deny"
   key_opts = [
-    "Decrypt",
-    "Encrypt",
-    "Sign",
-    "UnwrapKey",
-    "Verify",
-    "WrapKey",
+    "decrypt","encrypt","sign","unwrapKey","verify","wrapKey"
   ]
 }
