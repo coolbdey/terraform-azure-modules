389beb28e1d0c3bb9e39638f76c0368041f3f782
Woody <29414889+woowil@users.noreply.github.com>
2022-04-07 13:51:20 +0200

    Update locals.tf

--- azurerm_windows_function_app/locals.tf
+++ azurerm_windows_function_app/locals.tf
@@ -4,7 +4,13 @@ locals {
     WEBSITE_ENABLE_SYNC_UPDATE_SITE = true
     WEBSITE_TIME_ZONE               = var.time_zone
   }
-  app_settings = merge(local.appsettings_default, var.app_settings)
+  appsettings_appinsights = var.app_insights.enabled ? {
+    APPINSIGHTS_INSTRUMENTATIONKEY             = var.app_insights.instrumentation_key
+    APPLICATIONINSIGHTS_CONNECTION_STRING      = var.app_insights.connection_string
+    ApplicationInsightsAgent_EXTENSION_VERSION = "~2"
+  } : {}
+  app_settings = merge(local.appsettings_default, local.appsettings_appinsights, var.app_settings)
+
   identity_ids = var.managed_identity_type == "UserAssigned" || var.managed_identity_type == "SystemAssigned, UserAssigned" ? toset(var.managed_identity_ids) : null
 
   is_consumption_plan = can(regex("Y1", data.azurerm_service_plan.sp.sku_name)) || can(regex("FunctionApp", data.azurerm_service_plan.sp.kind))
