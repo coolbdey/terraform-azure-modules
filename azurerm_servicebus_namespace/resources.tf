05577c834acfa437083d79e31205a00598ceb518
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-04-08 00:06:35 +0200

    Update 04/08/2022 00:06:35

--- azurerm_servicebus_namespace/resources.tf
+++ azurerm_servicebus_namespace/resources.tf
@@ -51,37 +51,39 @@ resource "azurerm_servicebus_queue" "queue" {
   depends_on = [azurerm_servicebus_namespace.sbns]
   count      = length(var.queues)
 
-  name = var.queues[count.index].name
-  namespace_id = azurerm_servicebus_namespace.sbns.id
+  name                  = var.queues[count.index].name
+  namespace_id          = azurerm_servicebus_namespace.sbns.id
   resource_group_name   = data.azurerm_resource_group.rg.name
-  lock_duration         = "PT2M" #  (Optional) The ISO 8601 timespan duration of a peek-lock; that is, the amount of time that the message is locked for other receivers. Maximum value is 5 minutes. Defaults to 1 minute (PT1M).
-  max_size_in_megabytes = 1024   # (Optional) Integer value which controls the size of memory allocated for the queue. For supported values see the "Queue or topic size" section of Service Bus Quotas. Defaults to 1024
+  lock_duration         = "PT2M"                    #  (Optional) The ISO 8601 timespan duration of a peek-lock; that is, the amount of time that the message is locked for other receivers. Maximum value is 5 minutes. Defaults to 1 minute (PT1M).
+  max_size_in_megabytes = 1024                      # (Optional) Integer value which controls the size of memory allocated for the queue. For supported values see the "Queue or topic size" section of Service Bus Quotas. Defaults to 1024
   enable_partitioning   = local.enable_partitioning # (Optional) Boolean flag which controls whether to enable the queue to be partitioned across multiple message brokers. Changing this forces a new resource to be created. Defaults to false for Basic and Standard. For Premium, it MUST be set to true.
   #forward_to # (Optional) The name of a Queue or Topic to automatically forward messages to. Please see the documentation for more information.
   # forward_dead_lettered_messages_to - (Optional) The name of a Queue or Topic to automatically forward dead lettered messages to.
- 
+
 }
 
 ###########################################################
 ####### ACCESS RULES
 
 resource "azurerm_servicebus_namespace_authorization_rule" "rule_listen" {
-  name                = "Listen"
-  namespace_name      = azurerm_servicebus_namespace.sbns.name
-  resource_group_name = data.azurerm_resource_group.rg.name
+  depends_on = [azurerm_servicebus_namespace.sbns, data.azurerm_resource_group.rg]
 
+  name                = "Listen"
+  namespace_id        = azurerm_servicebus_namespace.sbns.id
+  resource_group_name = data.azurerm_resource_group.rg.name
   listen = true
   send   = false
   manage = false
 }
 resource "azurerm_servicebus_namespace_authorization_rule" "rule_send" {
-  name                = "Send"
-  namespace_name      = azurerm_servicebus_namespace.sbns.name
-  resource_group_name = data.azurerm_resource_group.rg.name
+  depends_on = [azurerm_servicebus_namespace.sbns, data.azurerm_resource_group.rg]
 
-  listen = false
-  send   = true
-  manage = false
+  name                = "Send"
+  namespace_id        = azurerm_servicebus_namespace.sbns.id
+  resource_group_name = data.azurerm_resource_group.rg.name
+  listen              = false
+  send                = true
+  manage              = false
 }
 resource "azurerm_servicebus_queue_authorization_rule" "rule_listen" {
   depends_on = [azurerm_servicebus_queue.queue]
@@ -89,12 +91,9 @@ resource "azurerm_servicebus_queue_authorization_rule" "rule_listen" {
 
   name     = "Listen"
   queue_id = azurerm_servicebus_queue.queue[count.index].id
-  #namespace_name      = azurerm_servicebus_namespace.sbns.name
-  #queue_name          = var.queues[count.index].name
-  #resource_group_name = data.azurerm_resource_group.rg.name
-  listen = var.queues[count.index].rule_manage ? true : var.queues[count.index].rule_listen
-  send   = var.queues[count.index].rule_manage ? true : false # When this property is true - both listen and send must be too.
-  manage = var.queues[count.index].rule_manage ? true : false # When this property is true - both listen and send must be too.
+  listen   = var.queues[count.index].rule_manage ? true : var.queues[count.index].rule_listen
+  send     = var.queues[count.index].rule_manage ? true : false # When this property is true - both listen and send must be too.
+  manage   = var.queues[count.index].rule_manage ? true : false # When this property is true - both listen and send must be too.
 
   lifecycle {
     ignore_changes = [queue_id]
@@ -107,12 +106,9 @@ resource "azurerm_servicebus_queue_authorization_rule" "rule_send" {
 
   name     = "Send"
   queue_id = azurerm_servicebus_queue.queue[count.index].id
-  #namespace_name      = azurerm_servicebus_namespace.sbns.name
-  #queue_name          = var.queues[count.index].name
-  #resource_group_name = data.azurerm_resource_group.rg.name
-  send   = var.queues[count.index].rule_manage ? true : var.queues[count.index].rule_send
-  listen = false
-  manage = false
+  send     = var.queues[count.index].rule_manage ? true : var.queues[count.index].rule_send
+  listen   = false
+  manage   = false
 
   lifecycle {
     ignore_changes = [queue_id]
@@ -125,13 +121,9 @@ resource "azurerm_servicebus_queue_authorization_rule" "rule_manage" {
 
   name     = "Manage"
   queue_id = azurerm_servicebus_queue.queue[count.index].id
-  #namespace_name      = azurerm_servicebus_namespace.sbns.name
-  #queue_name          = var.queues[count.index].name
-  #resource_group_name = data.azurerm_resource_group.rg.name
-
-  manage = var.queues[count.index].rule_manage
-  listen = var.queues[count.index].rule_manage ? true : true # Error: One of the `listen`, `send` or `manage` properties needs to be set
-  send   = var.queues[count.index].rule_manage ? true : false
+  manage   = var.queues[count.index].rule_manage
+  listen   = var.queues[count.index].rule_manage ? true : true # Error: One of the `listen`, `send` or `manage` properties needs to be set
+  send     = var.queues[count.index].rule_manage ? true : false
 
   lifecycle {
     ignore_changes = [queue_id]
--- azurerm_storage_account/locals.tf
+++ azurerm_storage_account/locals.tf
@@ -1,5 +1,5 @@
 locals {
   is_selected_networks              = var.network_rules.default_action == "Deny"
   account_tier                      = can(regex("BlockBlobStorage|FileStorage", var.account_kind)) ? "Premium" : var.account_tier
-  infrastructure_encryption_enabled = var.account_kind == "StorageV2" || (local.access_tier == "Premium" || var.account_kind == "BlockBlobStorage") ? var.infrastructure_encryption_enabled : false
+  infrastructure_encryption_enabled = var.account_kind == "StorageV2" || (var.access_tier == "Premium" || var.account_kind == "BlockBlobStorage") ? var.infrastructure_encryption_enabled : false
 }
